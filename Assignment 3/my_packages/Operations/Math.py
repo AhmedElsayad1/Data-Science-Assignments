b76a3602964982490e40ee051b4f1ed677453a15
Ahmed <ahmedelsayad2223@gmail.com>
2021-02-03 12:01:39 +0200

    Assignment 3
    Functions Task and Python Statements Task

---
+++ Assignment 3/my_packages/Operations/Math.py
@@ -0,0 +1,2 @@
+def get_average (num):
+    return sum(num) / len(num) 
---
+++ Assignment 3/my_packages/Transformers/StringTransformer.py
@@ -0,0 +1,6 @@
+def reverse_string(string):
+    return string[::-1]
+
+def capitalize_string(string):
+    return string.capitalize()
+
